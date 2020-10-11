e6bfc88f5ec6c5fdbc3967a431d5a0ec48940b3a
Dastan Abykeev <dastansulaimankulov98@gmail.com>
2020-10-12 01:01:28 +0600

    orders block finished

--- src/index.js
+++ src/index.js
@@ -1,2 +1,20 @@
+import Swiper, {Navigation, Pagination} from 'swiper';
 import './scss/index.scss';
+Swiper.use([Navigation, Pagination]);
 
+const swiper = new Swiper('.swiper-container', {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 1000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
