3dccdd33bf5cf076989e0bb80ec6813c37c1371b
Dastan Abykeev <dastansulaimankulov98@gmail.com>
2020-10-13 10:47:11 +0600

    finished about block

--- src/index.js
+++ src/index.js
@@ -5,10 +5,6 @@ Swiper.use([Navigation, Pagination]);
 const swiper = new Swiper('.swiper-container', {
   spaceBetween: 30,
   centeredSlides: true,
-  autoplay: {
-    delay: 1000,
-    disableOnInteraction: false,
-  },
   pagination: {
     el: '.swiper-pagination',
     clickable: true,
@@ -17,4 +13,52 @@ const swiper = new Swiper('.swiper-container', {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
+});
+
+const swiper2 = new Swiper('.circle-container', {
+  spaceBetween: 30,
+  centeredSlides: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 30,
+    },
+    768: {
+      centeredSlides: false,
+      slidesPerView: 4,
+      spaceBetween: 0,
+    },
+  }
+});
+
+const swiper3 = new Swiper('.choice-container', {
+  spaceBetween: 30,
+  centeredSlides: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 30,
+    },
+    768: {
+      centeredSlides: false,
+      slidesPerView: 2,
+      spaceBetween: 0,
+    },
+  }
 });
